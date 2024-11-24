6d0646886616796e629677049b041fd13bb0a0ee
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-11-24 18:26:26 +0300

    spring security: защита веб-запросов

--- taco-cloud/src/main/java/tacos/config/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/config/SecurityConfig.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import tacos.data.User;
+import tacos.repo.UserRepository;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,20 +20,18 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
 
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        List<UserDetails> usersList = new ArrayList<>();
-        usersList.add(new User(
-                "buzz", encoder.encode("password"),
-                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-        usersList.add(new User(
-                "woody", encoder.encode("password"),
-                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-        return new InMemoryUserDetailsManager(usersList);
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> {
+            User user = userRepository.findByUsername(username);
+            if (user == null) return user;
+            throw new UsernameNotFoundException("User " + username + "not found");
+        };
     }
 }
--- taco-cloud/src/main/java/tacos/service/UserDetailsImpl.java
+++ taco-cloud/src/main/java/tacos/service/UserDetailsImpl.java
@@ -4,10 +4,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public class UserDetailsImpl implements UserDetailsService {
-    @Override
-    public UserDetails loadUserByUsername(String username)
-            throws UsernameNotFoundException {
-        return null;
-    }
+public class UserDetailsImpl {
+//        implements UserDetailsService {
+//    @Override
+//    public UserDetails loadUserByUsername(String username)
+//            throws UsernameNotFoundException {
+//        return null;
+//    }
 }
