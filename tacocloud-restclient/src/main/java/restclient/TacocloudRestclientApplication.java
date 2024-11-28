57b779632b7548bbfbf38ea7e4e5ffe63c53eb6f
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-11-28 11:10:38 +0300

    add requests to rest-client

--- tacocloud-restclient/src/main/java/restclient/TacocloudRestclientApplication.java
+++ tacocloud-restclient/src/main/java/restclient/TacocloudRestclientApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import restclient.data.Client;
+import restclient.data.Ingredient;
 
 @SpringBootApplication
 @Slf4j
@@ -22,13 +23,28 @@ public class TacocloudRestclientApplication {
 	}
 
 
+//	@Bean
+//	public CommandLineRunner fetchIngredients(Client tacoCloudClient) {
+//		return args -> {
+//			log.info("----------------------- GET -------------------------");
+//			log.info("GETTING INGREDIENT BY IDE");
+//			log.info("Ingredient:  " + tacoCloudClient.getIngredientById_response("CHED"));
+//		};
+//	}
+
 	@Bean
-	public CommandLineRunner fetchIngredients(Client tacoCloudClient) {
+	public CommandLineRunner addAnIngredient(Client tacoCloudClient) {
 		return args -> {
-			log.info("----------------------- GET -------------------------");
-			log.info("GETTING INGREDIENT BY IDE");
-			log.info("Ingredient:  " + tacoCloudClient.getIngredientById("CHED"));
+			log.info("----------------------- POST -------------------------");
+			Ingredient chix = new Ingredient("CHIX", "Shredded Chicken", Ingredient.Type.PROTEIN);
+			Ingredient chixAfter = tacoCloudClient.createIngredient(chix);
+			log.info("AFTER=1:  " + chixAfter);
+//      Ingredient beefFajita = new Ingredient("BFFJ", "Beef Fajita", Ingredient.Type.PROTEIN);
+//      URI uri = tacoCloudClient.createIngredient(beefFajita);
+//      log.info("AFTER-2:  " + uri);
+//      Ingredient shrimp = new Ingredient("SHMP", "Shrimp", Ingredient.Type.PROTEIN);
+//      Ingredient shrimpAfter = tacoCloudClient.createIngredient(shrimp);
+//      log.info("AFTER-3:  " + shrimpAfter);
 		};
 	}
-
 }
--- tacocloud-restclient/src/main/java/restclient/data/Client.java
+++ tacocloud-restclient/src/main/java/restclient/data/Client.java
@@ -2,6 +2,7 @@ package restclient.data;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,5 +20,19 @@ public class Client {
     public Ingredient getIngredientById(String ingredientId) {
         return rest.getForObject("http://localhost:8080/data-api/ingredients/{id}",
                 Ingredient.class, ingredientId);
+    }
+
+    public Ingredient getIngredientById_response(String ingredientId) {
+        ResponseEntity<Ingredient> responseEntity =
+                rest.getForEntity("http://localhost:8080/data-api/ingredients/{id}",
+                        Ingredient.class, ingredientId);
+        log.info("Fetched time: {}",
+                responseEntity.getHeaders().getDate());
+        return responseEntity.getBody();
+    }
+
+    public Ingredient createIngredient(Ingredient ingredient) {
+        return rest.postForObject("http://localhost:8080/data-api/ingredients",
+                ingredient, Ingredient.class);
     }
 }
